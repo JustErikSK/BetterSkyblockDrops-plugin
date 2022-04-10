6b66291afc5d7fbe51952de1b86adf85a888ea26
JustErikSK <sebo.erik@gmail.com>
2022-04-10 13:26:00 +0200

    OriginalSkyblockDrops

--- src/main/java/me/minecraft/plugin/skyblockdrops/MobDrops.java
+++ src/main/java/me/minecraft/plugin/skyblockdrops/MobDrops.java
@@ -1,12 +1,14 @@
 package me.minecraft.plugin.skyblockdrops;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Random;
 
@@ -15,40 +17,59 @@ public class MobDrops implements Listener {
     @EventHandler
     public void onMobDeath(EntityDeathEvent event) {
 
+        JavaPlugin plugin = null;
+        FileConfiguration config = plugin.getConfig();
+
+        int gravel_perc = plugin.getConfig().getInt("gravel_perc", 20);
+        int gravel_amount = plugin.getConfig().getInt("gravel_amount", 1);
+        int sand_perc = plugin.getConfig().getInt("sand_perc", 20);
+        int sand_amount = plugin.getConfig().getInt("sand_amount", 1);
+        int quartz_perc = plugin.getConfig().getInt("quartz_perc", 20);
+        int quartz_amount = plugin.getConfig().getInt("quartz_amount", 1);
+        int soul_sand_perc = plugin.getConfig().getInt("soul_sand_perc", 20);
+        int soul_sand_amount = plugin.getConfig().getInt("soul_sand_amount", 1);
+        int ancient_debris_perc = plugin.getConfig().getInt("ancient_debris_perc", 100);
+        int ancient_debris_amount = plugin.getConfig().getInt("ancient_debris_amount", 2);
+        int magma_cream_perc = plugin.getConfig().getInt("magma_cream_perc", 15);
+        int magma_cream_amount = plugin.getConfig().getInt("magma_cream_amount", 1);
+
         LivingEntity entity = event.getEntity();
         Random random = new Random();
-        ItemStack drop1 = new ItemStack(Material.GRAVEL,1);
-        ItemStack drop2 = new ItemStack(Material.QUARTZ,1);
-        ItemStack drop3 = new ItemStack(Material.SAND,1);
-        ItemStack drop4 = new ItemStack(Material.SOUL_SAND,1);
-        ItemStack drop5 = new ItemStack(Material.ANCIENT_DEBRIS,2);
-        ItemStack drop6 = new ItemStack(Material.MAGMA_CREAM, 1);
+        ItemStack drop1 = new ItemStack(Material.GRAVEL, gravel_amount);
+        ItemStack drop3 = new ItemStack(Material.SAND, sand_amount);
+        ItemStack drop2 = new ItemStack(Material.QUARTZ, quartz_amount);
+        ItemStack drop4 = new ItemStack(Material.SOUL_SAND, soul_sand_amount);
+        ItemStack drop5 = new ItemStack(Material.ANCIENT_DEBRIS, ancient_debris_amount);
+        ItemStack drop6 = new ItemStack(Material.MAGMA_CREAM, magma_cream_amount);
 
         if (entity.getType() == EntityType.ZOMBIE) {
             int number1 = random.nextInt(100);
-            if (number1 < 20) {
+            if (number1 < gravel_perc) {
                 event.getDrops().add(drop1);
             }
-        } else if (entity.getType() == EntityType.BLAZE) {
+        } else if (entity.getType() == EntityType.HUSK) {
             int number2 = random.nextInt(100);
-            if (number2 < 20) {
+            if (number2 < sand_perc) {
                 event.getDrops().add(drop2);
             }
-        } else if (entity.getType() == EntityType.HUSK) {
+        } else if (entity.getType() == EntityType.BLAZE) {
             int number3 = random.nextInt(100);
-            if (number3 < 20) {
+            if (number3 < quartz_perc) {
                 event.getDrops().add(drop3);
             }
         } else if (entity.getType() == EntityType.WITHER_SKELETON) {
             int number4 = random.nextInt(100);
-            if (number4 < 20) {
+            if (number4 < soul_sand_perc) {
                 event.getDrops().add(drop4);
             }
         } else if (entity.getType() == EntityType.WITHER) {
-            event.getDrops().add(drop5);
-        } else if (entity.getType() == EntityType.ZOMBIFIED_PIGLIN) {
             int number5 = random.nextInt(100);
-            if (number5 < 15) {
+            if (number5 < ancient_debris_perc) {
+                event.getDrops().add(drop5);
+            }
+        } else if (entity.getType() == EntityType.ZOMBIFIED_PIGLIN) {
+            int number6 = random.nextInt(100);
+            if (number6 < magma_cream_perc) {
                 event.getDrops().add(drop6);
             }
         }
--- src/main/java/me/minecraft/plugin/skyblockdrops/Skyblockdrops.java
+++ src/main/java/me/minecraft/plugin/skyblockdrops/Skyblockdrops.java
@@ -2,25 +2,91 @@ package me.minecraft.plugin.skyblockdrops;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Skyblockdrops extends JavaPlugin {
+import java.util.Random;
+
+public final class Skyblockdrops extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        Bukkit.broadcastMessage(ChatColor.GREEN + this.getName() + " >> Plugin has been enabled!");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "CONSOLE ONLY : " + this.getName() + " >> Plugin has been enabled!");
+        Bukkit.broadcastMessage(ChatColor.GREEN + "OriginalSkyblockDrops >> Plugin has been enabled!");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "CONSOLE ONLY : OriginalSkyblockDrops >> Plugin has been enabled!");
         Bukkit.broadcastMessage(ChatColor.YELLOW + "List of drops: Zombies drop gravel, Husks drop sand, Blazes drop quartz," +
                 " Wither Skeletons drop soul sand, Withers drop ancient debris and Zombified Piglins drop magma cream!");
-        this.getServer().getPluginManager().registerEvents(new MobDrops(), this);
+        //this.getServer().getPluginManager().registerEvents(new MobDrops(), this);
+        this.getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
+
+        FileConfiguration config = this.getConfig();
+        config.addDefault("gravel_perc", 20);
+        config.addDefault("gravel_amount", 1);
+        config.addDefault("sand_perc", 20);
+        config.addDefault("sand_amount", 1);
+        config.addDefault("quartz_perc", 20);
+        config.addDefault("quartz_amount", 1);
+        config.addDefault("soul_sand_perc", 20);
+        config.addDefault("soul_sand_amount", 1);
+        config.addDefault("ancient_debris_perc", 100);
+        config.addDefault("ancient_debris_amount", 2);
+        config.addDefault("magma_cream_perc", 15);
+        config.addDefault("magma_cream_amount", 1);
+        config.addDefault("reload_message","&9Reloaded OriginalSkyblockDrops Config");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.broadcastMessage(ChatColor.RED + this.getName() + " >> Plugin has been disabled!");
-        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "CONSOLE ONLY : " + this.getName() + " >> Plugin has been disabled!");
+        Bukkit.broadcastMessage(ChatColor.RED + "OriginalSkyblockDrops >> Plugin has been disabled!");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "CONSOLE ONLY : OriginalSkyblockDrops >> Plugin has been disabled!");
         this.getServer().getPluginManager().disablePlugin(this);
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (label.equalsIgnoreCase("skyblockdrops")) {
+            if (!sender.hasPermission("skyblockdrops.reload")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permissions to use this command!");
+                return true;
+            }
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.RED + "Usage: /skyblockdrops reload");
+                return true;
+            }
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("reload")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            this.getConfig().getString("reload_message")));
+                }
+                    this.reloadConfig();
+            }
+        }return false;
+    }
+
+    @EventHandler
+    public void onMobKill(EntityDeathEvent e) {
+
+        int gravel_perc = this.getConfig().getInt("gravel_perc", 20);
+        int gravel_amount = this.getConfig().getInt("gravel_amount", 1);
+
+        LivingEntity entity = e.getEntity();
+        Random random = new Random();
+
+        ItemStack drop1 = new ItemStack(Material.GRAVEL, gravel_amount);
+
+        if (entity.getType() == EntityType.ZOMBIE) {
+            int number1 = random.nextInt(100);
+            if (number1 < gravel_perc) {
+                e.getDrops().add(drop1);
+            }
+        }
+    }
 }
