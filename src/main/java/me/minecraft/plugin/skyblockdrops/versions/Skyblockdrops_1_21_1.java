f355d50ddfbb51b84e4c64c5b49c009b8c556f31
juste <sebo.erik@gmail.com>
2024-10-13 17:57:04 +0200

    changed disable rule to drop rule in config.yml and the main class. initial commit for halloween event rule

--- src/main/java/me/minecraft/plugin/skyblockdrops/versions/Skyblockdrops_1_21_1.java
+++ src/main/java/me/minecraft/plugin/skyblockdrops/versions/Skyblockdrops_1_21_1.java
@@ -50,33 +50,33 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
         //  GET DEFAULT VALUES FOR ALL DROPS
         int gravel_perc = this.getConfig().getInt("gravel_perc", 20);
         int gravel_amount = this.getConfig().getInt("gravel_amount", 1);
-        String gravel_disable = this.getConfig().getString("gravel_disable", "false");
+        String gravel_drop = this.getConfig().getString("gravel_drop", "true");
         int sand_perc = this.getConfig().getInt("sand_perc", 20);
         int sand_amount = this.getConfig().getInt("sand_amount", 1);
-        String sand_disable = this.getConfig().getString("sand_disable", "false");
+        String sand_drop = this.getConfig().getString("sand_drop", "true");
         int quartz_perc = this.getConfig().getInt("quartz_perc", 20);
         int quartz_amount = this.getConfig().getInt("quartz_amount", 1);
-        String quartz_disable = this.getConfig().getString("quartz_disable", "false");
+        String quartz_drop = this.getConfig().getString("quartz_drop", "true");
         int soul_sand_perc = this.getConfig().getInt("soul_sand_perc", 20);
         int soul_sand_amount = this.getConfig().getInt("soul_sand_amount", 1);
-        String soul_sand_disable = this.getConfig().getString("soul_sand_disable", "false");
+        String soul_sand_drop = this.getConfig().getString("soul_sand_drop", "true");
         int ancient_debris_perc = this.getConfig().getInt("ancient_debris_perc", 100);
         int ancient_debris_amount = this.getConfig().getInt("ancient_debris_amount", 4);
-        String ancient_debris_disable = this.getConfig().getString("ancient_debris_disable", "false");
+        String ancient_debris_drop = this.getConfig().getString("ancient_debris_drop", "true");
         int magma_cream_perc = this.getConfig().getInt("magma_cream_perc", 15);
         int magma_cream_amount = this.getConfig().getInt("magma_cream_amount", 1);
-        String magma_cream_disable = this.getConfig().getString("magma_cream_disable", "false");
+        String magma_cream_drop = this.getConfig().getString("magma_cream_drop", "true");
 
         LivingEntity entity = e.getEntity();
         Random random = new Random();
 
         int number = random.nextInt(100);
 
-        // DROPS WILL WORK ONLY IF THE DISABLE OPTION IS SET TO FALSE IN CONFIG.YML
+        // DROPS WILL WORK ONLY IF THE DROP OPTION IS SET TO TRUE IN CONFIG.YML
         // IF PERCENTAGE IS MORE THAN 100 OR LESS THAN 1, DEFAULT VALUE WILL BE ACTIVE
         // IF AMOUNT IS MORE THAN 10 OR LESS THAN 1, ONLY VANILLA DROPS WILL BE APPLIED
 
-        if (entity.getType() == EntityType.ZOMBIE && gravel_disable.equals("false")) { // GRAVEL
+        if (entity.getType() == EntityType.ZOMBIE && gravel_drop.equals("true")) { // GRAVEL
             if (gravel_perc > 100 || gravel_perc < 1) {
                 gravel_perc = 20; // USE DEFAULT PERCENTAGE
             }
@@ -88,7 +88,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
             }
         }
 
-        if (entity.getType() == EntityType.HUSK && sand_disable.equals("false")) { // SAND
+        if (entity.getType() == EntityType.HUSK && sand_drop.equals("true")) { // SAND
             if (sand_perc > 100 || sand_perc < 1) {
                 sand_perc = 20; // USE DEFAULT PERCENTAGE
             }
@@ -100,7 +100,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
             }
         }
 
-        if (entity.getType() == EntityType.BLAZE && quartz_disable.equals("false")) { // QUARTZ
+        if (entity.getType() == EntityType.BLAZE && quartz_drop.equals("true")) { // QUARTZ
             if (quartz_perc > 100 || quartz_perc < 1) {
                 quartz_perc = 20; // USE DEFAULT PERCENTAGE
             }
@@ -112,7 +112,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
             }
         }
 
-        if (entity.getType() == EntityType.WITHER_SKELETON && soul_sand_disable.equals("false")) { // SOUL SAND
+        if (entity.getType() == EntityType.WITHER_SKELETON && soul_sand_drop.equals("true")) { // SOUL SAND
             if (soul_sand_perc > 100 || soul_sand_perc < 1) {
                 soul_sand_perc = 20; // USE DEFAULT PERCENTAGE
             }
@@ -124,7 +124,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
             }
         }
 
-        if (entity.getType() == EntityType.WITHER && ancient_debris_disable.equals("false")) { // ANCIENT DEBRIS
+        if (entity.getType() == EntityType.WITHER && ancient_debris_drop.equals("true")) { // ANCIENT DEBRIS
             if (ancient_debris_perc > 100 || ancient_debris_perc < 1) {
                 ancient_debris_perc = 100; // USE DEFAULT PERCENTAGE
             }
@@ -136,7 +136,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
             }
         }
 
-        if (entity.getType() == EntityType.ZOMBIFIED_PIGLIN && magma_cream_disable.equals("false")) { // MAGMA CREAM
+        if (entity.getType() == EntityType.ZOMBIFIED_PIGLIN && magma_cream_drop.equals("true")) { // MAGMA CREAM
             if (magma_cream_perc > 100 || magma_cream_perc < 1) {
                 magma_cream_perc = 15; // USE DEFAULT PERCENTAGE
             }
@@ -147,5 +147,12 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
                 e.getDrops().add(new ItemStack(Material.MAGMA_CREAM, magma_cream_amount)); // MAGMA CREAM DROP
             }
         }
+    }
+
+    @EventHandler
+    public void halloweenEvent(EntityDeathEvent event) {
+
+        // GET THE DEFAULT VALUE FOR EVENT RULES
+        String halloween_event = this.getConfig().getString("halloween_event", "true");
     }
 }
