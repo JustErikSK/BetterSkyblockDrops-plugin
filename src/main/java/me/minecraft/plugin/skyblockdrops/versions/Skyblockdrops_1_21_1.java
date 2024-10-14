d4dc74e1b9909e9deabbffbd96aa79aa56b003c5
juste <sebo.erik@gmail.com>
2024-10-14 20:10:14 +0200

    added new defaults for config and changed some default values

--- src/main/java/me/minecraft/plugin/skyblockdrops/versions/Skyblockdrops_1_21_1.java
+++ src/main/java/me/minecraft/plugin/skyblockdrops/versions/Skyblockdrops_1_21_1.java
@@ -25,46 +25,50 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
         saveDefaultConfig();
 
         FileConfiguration config = this.getConfig();
-        config.addDefault("gravel_perc", 20);
+        config.addDefault("gravel_perc", 10);
         config.addDefault("gravel_amount", 1);
-        config.addDefault("gravel_disable", false);
-        config.addDefault("sand_perc", 20);
+        config.addDefault("gravel_drop", true);
+        config.addDefault("sand_perc", 10);
         config.addDefault("sand_amount", 1);
-        config.addDefault("sand_disable", false);
-        config.addDefault("quartz_perc", 20);
+        config.addDefault("sand_drop", true);
+        config.addDefault("quartz_perc", 10);
         config.addDefault("quartz_amount", 1);
-        config.addDefault("quartz_disable", false);
-        config.addDefault("soul_sand_perc", 20);
+        config.addDefault("quartz_drop", true);
+        config.addDefault("soul_sand_perc", 10);
         config.addDefault("soul_sand_amount", 1);
-        config.addDefault("soul_sand_disable", false);
+        config.addDefault("soul_sand_drop", true);
         config.addDefault("ancient_debris_perc", 100);
         config.addDefault("ancient_debris_amount", 4);
-        config.addDefault("ancient_debris_disable", false);
-        config.addDefault("magma_cream_perc", 15);
+        config.addDefault("ancient_debris_drop", true);
+        config.addDefault("magma_cream_perc", 10);
         config.addDefault("magma_cream_amount", 1);
-        config.addDefault("magma_cream_disable", false);
+        config.addDefault("magma_cream_drop", true);
+        config.addDefault("glowstone_perc", 10);
+        config.addDefault("glowstone_amount", 1);
+        config.addDefault("glowstone_drop", true);
+        config.addDefault("halloween_event", true);
     }
 
     @EventHandler
     public void onMobKill(EntityDeathEvent e) {
 
         //  GET DEFAULT VALUES FOR ALL DROPS
-        int gravel_perc = this.getConfig().getInt("gravel_perc", 20);
+        int gravel_perc = this.getConfig().getInt("gravel_perc", 10);
         int gravel_amount = this.getConfig().getInt("gravel_amount", 1);
         String gravel_drop = this.getConfig().getString("gravel_drop", "true");
-        int sand_perc = this.getConfig().getInt("sand_perc", 20);
+        int sand_perc = this.getConfig().getInt("sand_perc", 10);
         int sand_amount = this.getConfig().getInt("sand_amount", 1);
         String sand_drop = this.getConfig().getString("sand_drop", "true");
-        int quartz_perc = this.getConfig().getInt("quartz_perc", 20);
+        int quartz_perc = this.getConfig().getInt("quartz_perc", 10);
         int quartz_amount = this.getConfig().getInt("quartz_amount", 1);
         String quartz_drop = this.getConfig().getString("quartz_drop", "true");
-        int soul_sand_perc = this.getConfig().getInt("soul_sand_perc", 20);
+        int soul_sand_perc = this.getConfig().getInt("soul_sand_perc", 10);
         int soul_sand_amount = this.getConfig().getInt("soul_sand_amount", 1);
         String soul_sand_drop = this.getConfig().getString("soul_sand_drop", "true");
         int ancient_debris_perc = this.getConfig().getInt("ancient_debris_perc", 100);
         int ancient_debris_amount = this.getConfig().getInt("ancient_debris_amount", 4);
         String ancient_debris_drop = this.getConfig().getString("ancient_debris_drop", "true");
-        int magma_cream_perc = this.getConfig().getInt("magma_cream_perc", 15);
+        int magma_cream_perc = this.getConfig().getInt("magma_cream_perc", 10);
         int magma_cream_amount = this.getConfig().getInt("magma_cream_amount", 1);
         String magma_cream_drop = this.getConfig().getString("magma_cream_drop", "true");
         int glowstone_perc = this.getConfig().getInt("glowstone_perc", 10);
@@ -82,7 +86,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
 
         if (entity.getType() == EntityType.ZOMBIE && gravel_drop.equals("true")) { // GRAVEL
             if (gravel_perc > 100 || gravel_perc < 1) {
-                gravel_perc = 20; // USE DEFAULT PERCENTAGE
+                gravel_perc = 10; // USE DEFAULT PERCENTAGE
             }
             if (gravel_amount > 10 || gravel_amount < 1) {
                 gravel_amount = 1; // USE DEFAULT AMOUNT
@@ -94,7 +98,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
 
         if (entity.getType() == EntityType.HUSK && sand_drop.equals("true")) { // SAND
             if (sand_perc > 100 || sand_perc < 1) {
-                sand_perc = 20; // USE DEFAULT PERCENTAGE
+                sand_perc = 10; // USE DEFAULT PERCENTAGE
             }
             if (sand_amount > 10 || sand_amount < 1) {
                 sand_amount = 1; // USE DEFAULT AMOUNT
@@ -106,7 +110,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
 
         if (entity.getType() == EntityType.BLAZE && quartz_drop.equals("true")) { // QUARTZ
             if (quartz_perc > 100 || quartz_perc < 1) {
-                quartz_perc = 20; // USE DEFAULT PERCENTAGE
+                quartz_perc = 10; // USE DEFAULT PERCENTAGE
             }
             if (quartz_amount > 10 || quartz_amount < 1) {
                 quartz_amount = 1; // USE DEFAULT AMOUNT
@@ -118,7 +122,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
 
         if (entity.getType() == EntityType.WITHER_SKELETON && soul_sand_drop.equals("true")) { // SOUL SAND
             if (soul_sand_perc > 100 || soul_sand_perc < 1) {
-                soul_sand_perc = 20; // USE DEFAULT PERCENTAGE
+                soul_sand_perc = 10; // USE DEFAULT PERCENTAGE
             }
             if (soul_sand_amount > 10 || soul_sand_amount < 1) {
                 soul_sand_amount = 1; // USE DEFAULT AMOUNT
@@ -142,7 +146,7 @@ public class Skyblockdrops_1_21_1 extends JavaPlugin implements Listener {
 
         if (entity.getType() == EntityType.ZOMBIFIED_PIGLIN && magma_cream_drop.equals("true")) { // MAGMA CREAM
             if (magma_cream_perc > 100 || magma_cream_perc < 1) {
-                magma_cream_perc = 15; // USE DEFAULT PERCENTAGE
+                magma_cream_perc = 10; // USE DEFAULT PERCENTAGE
             }
             if (magma_cream_amount > 10 || magma_cream_amount < 1) {
                 magma_cream_amount = 1; // USE DEFAULT AMOUNT
